76fb54482a24bb19cb1eb0ab26761837b55ee5b4
PDXMelon <owen.c.lee.it@gmail.com>
2024-03-14 14:28:44 -0700

    owo

--- app.js
+++
@@ -1,8 +0,0 @@
-if ("serviceWorker" in navigator) {
-    window.addEventListener("load", function() {
-      navigator.serviceWorker
-        .register("/pwa-test/worker.js")
-        .then(res => alert("service worker registered"))
-        .catch(err => alert("service worker not registered. Error: "+ err))
-    })
-  }
