10ac6d30c1709e0abd7cbb5d8c942c5ab74dc8cb
PDXMelon <owen.c.lee.it@gmail.com>
2024-03-14 10:50:17 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+const registerServiceWorker = async () => {
+  if ("serviceWorker" in navigator) {
+    try {
+      const registration = await navigator.serviceWorker.register("/service-worker.js", {
+        scope: "/",
+      });
+      if (registration.installing) {
+        alert("Service worker installing");
+      } else if (registration.waiting) {
+        alert("Service worker installed");
+      } else if (registration.active) {
+        alert("Service worker active");
+      }
+    } catch (error) {
+      alert(`Registration failed with ${error}`);
+    }
+  }
+};
+
+// …
+
+registerServiceWorker();
